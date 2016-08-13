0cbaecca661a5aec828ab1da333d9562bc55ad56
Aakash Arora <aakasharora0903@gmail.com>
2016-08-13 17:20:12 +0530

    Create update.py

---
+++ update.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import glob
+
+
+
+# define similarly
+def listing_banner_street_view(lat, lng):
+	return "a:5:{s:8:\"latitude\";s:9:\""+str(lat)+"\";s:9:\"longitude\";s:19:\""+str(lng)+"\";s:4:\"zoom\";s:1:\"1\";s:7:\"heading\";s:3:\"-18\";s:5:\"pitch\";s:2:\"25\";}"
+
+def listing_street_view_location(lat, lng):
+	return "a:5:{s:8:\"latitude\";s:9:\""+str(lat)+"\";s:9:\"longitude\";s:19:\""+str(lng)+"\";s:4:\"zoom\";s:1:\"1\";s:7:\"heading\";s:3:\"-18\";s:5:\"pitch\";s:2:\"25\";}"
+
+
+files_li = glob.glob("processed_*.csv") # update path here
+fields = ['lat', 'lng'] #for filling lat and lng
+
+count = 1
+for file in files_li:
+
+	csv_input = pd.read_csv(file)
+
+	###############
+	# adding cols #
+	###############
+	csv_input['listing_banner_map_marker'] = 'on'
+	csv_input['listing_banner_map_type'] = 'ROADMAP'
+	csv_input['listing_banner_map_zoom'] = '16'
+	csv_input['listing_banner'] = 'banner_map'
+	csv_input['listing_inside_view_location'] = ''
+	csv_input['listing_inside_view_location_latitude'] = csv_input['lat']
+	csv_input['listing_inside_view_location_longitude'] = csv_input['lng']
+	csv_input['listing_inside_view_location_zoom'] = '16'
+	csv_input['listing_google_street_view_latitude'] = csv_input['lat']
+	csv_input['listing_google_street_view_longitude'] = csv_input['lng']
+	csv_input['listing_google_street_view_zoom'] = '16'
+	csv_input['listing_banner_street_view_latitude'] = csv_input['lat']
+	csv_input['listing_banner_street_view_longitude'] = csv_input['lng']
+	csv_input['listing_banner_street_view_zoom'] = '16'
+	csv_input['post_name'] = csv_input['Name']	# TO assign values of 1 col to new col
+	csv_input['listing_description'] = csv_input['Details']	# TO assign values of 1 col to new col
+	csv_input['listing_address'] = csv_input['fullAddress']
+	csv_input['listing_email'] = csv_input['Mail']
+	csv_input['listing_featured_image'] = csv_input['featured_image']
+	csv_input['listing_gallery'] = csv_input['Images URL']
+	csv_input['listing_map_location'] = csv_input['fullAddress']
+	csv_input['listing_map_location_latitude'] = csv_input['lat']
+	csv_input['listing_map_location_longitude'] = csv_input['lng']
+	csv_input['listing_map_location_zoom'] = '16'
+	csv_input['listing_phone'] = csv_input['Phone1']
+	csv_input['listing_street_view_location'] = csv_input['fullAddress']
+	csv_input['listing_street_view_latitude'] = csv_input['lat']
+	csv_input['listing_street_view_longitude'] = csv_input['lng']
+ 	csv_input['listing_street_view_zoom'] = '16'
+	csv_input['listing_street_view_location'] = csv_input['fullAddress']
+	csv_input['listing_street_view_location_latitude'] = csv_input['lat']
+	csv_input['listing_street_view_location_longitude'] = csv_input['lng']
+	csv_input['listing_street_view_location_zoom'] = '16'
+ 	csv_input['listing_type_metabox'] = csv_input['Services Offered']
+ 	csv_input['locations'] = csv_input['listing_locations']
+  	#csv_input['listing_google_address']
+	#csv_input['listing_google_map'] = csv_input['Address']
+	#csv_input['listing_phone'] 
+	#csv_input['listing_inside_view_location']
+	#csv_input['listing_map_location'] = csv_input['Address']
+	#csv_input['listing_street_view_location']
+
+	csv_input['post_category'] = csv_input['Services Offered']
+	csv_input['post_tag'] = csv_input['Services Offered']
+	csv_input['listing_categories'] = csv_input['Services Offered']
+
+	############
+	# renaming #
+	############
+	csv_input.rename( columns = {
+									'Name'   :  'post_title',
+									'Details'  :  'post_content',
+									'Mail'   :  'claim_email',
+									'listing_person' : 'claim_name',
+									'Phone1' : 'claim_phone'
+									###phone
+									###listing_description
+									#'Address'
+
+								}, inplace = True )
+	
+	print(count)
+	count+=1
+
+	##################################################
+	# create lists here same no. as cols to be added #
+	##################################################
+	li1 = []; li2 = []
+
+	print file + ' -> ' + 'updated_'+file[file.find('processed_')+10:]
+	csv_input = pd.read_csv(file)
+	#print csv_input.keys()
+	for x in range(len(csv_input.lat)):
+
+		####################
+		# create col first #
+		####################
+		csv_input['listing_banner_street_view'] = ''
+		csv_input['listing_street_view_location'] = ''
+
+		#############################
+		# call functions from here  #
+		#############################
+		li1.append(listing_banner_street_view(csv_input.lat[x], csv_input.lng[x]))
+		li2.append(listing_street_view_location(csv_input.lat[x], csv_input.lng[x]))
+		#csv_input.loc[ x ,'listing_banner_street_view'] = listing_banner_street_view(csv_input.lat[x], csv_input.lng[x])
+	
+	#######################
+	# finally change here #
+	#######################
+	for x in range(len(li1)):
+		csv_input.loc[ x ,'listing_banner_street_view'] = li1[x]
+		csv_input.loc[ x ,'listing_street_view_location'] = li2[x]
+
+	'''
+	# check values of col using this
+	for y in csv_input.listing_banner_street_view:
+		print y
+		print '\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n'
+	'''
+
+csv_input.to_csv('updated_'+file[file.find('processed_')+10:], index=False)
+
